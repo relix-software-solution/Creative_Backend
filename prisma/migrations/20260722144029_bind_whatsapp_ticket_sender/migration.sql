d7f770d51f5aaed023e4f8a686c00748d8aa9d6a
MoShero-13 <mo3206213@gmail.com>
2026-07-23 17:49:29 +0300

    Fix migration table casing for production MySQL

--- prisma/migrations/20260722144029_bind_whatsapp_ticket_sender/migration.sql
+++ prisma/migrations/20260722144029_bind_whatsapp_ticket_sender/migration.sql
@@ -1,5 +1,5 @@
 -- AlterTable
-ALTER TABLE `registration` ADD COLUMN `ticketRequestClaimedAt` DATETIME(3) NULL,
+ALTER TABLE `Registration` ADD COLUMN `ticketRequestClaimedAt` DATETIME(3) NULL,
     ADD COLUMN `ticketRequestPhone` VARCHAR(191) NULL;
 
 -- CreateIndex
